758b89b06b99ef2eaa63ec97ef2c8a1e7bbe1e48
Matthijs Reukers <matthijsr@users.noreply.github.com>
2021-10-07 20:35:30 +0200

    Plot LAM_prd graph using distance formula, as well.

--- main.py
+++ main.py
@@ -33,7 +33,6 @@ def test():
     print(f"T_prop (assume alpha=2, z_0=10, d=50, N=3800, L=5000): {formulas.T_prop_assum_Dmin(50, LAM_size, 3800, BW, 5000)}")
     print(f"T_prop (assume alpha=2, z_0=10, d=50, N=4100, L=5000): {formulas.T_prop_assum_Dmin(50, LAM_size, 4100, BW, 5000)}")
 
-
 def plot_LAM_period(func, saveloc, LAM_size=350):
     BW = 1024 * 1024
     alpha = 2
@@ -53,6 +52,42 @@ def plot_LAM_period(func, saveloc, LAM_size=350):
     y5 = [func(L, 25, f_prd, BW, LAM_size, lm) for L in x]
     plt.plot(x, y5, label='25sec LAM period', linestyle='dashdot', color='grey')
     y6 = [func(L, 30, f_prd, BW, LAM_size, lm) for L in x]
+    plt.plot(x, y6, label='30sec LAM period', linestyle=(0, (3, 1, 1, 1, 1, 1)), color='orange')
+
+    # plt.title(f'Maximum number of nodes for time required to propagate LAMs. LAM_size = {LAM_size}')
+
+    plt.xlabel('Area Length/Width (L) in meters')
+    plt.ylabel('Number of Nodes')
+    plt.yticks(range(0, 8001, 1000))
+
+    # Add a grid
+    plt.grid(alpha=.4)
+
+    # Add a Legend
+    plt.legend()
+
+    # Show the plot
+    plt.savefig(saveloc, bbox_inches='tight')
+    plt.close()
+
+
+def plot_LAM_period_distance(d, saveloc, LAM_size=350):
+    BW = 1024 * 1024
+    f_prd = 0.1
+    T_prop = lambda LAM_prd: LAM_prd * f_prd
+
+    x = np.array(range(1000, 5001, 500))
+    y1 = [formulas.N_max_d(T_prop(5), BW, L, d, LAM_size) for L in x]
+    plt.plot(x, y1, label='05sec LAM period', linestyle='solid', color='red')
+    y2 = [formulas.N_max_d(T_prop(10), BW, L, d, LAM_size) for L in x]
+    plt.plot(x, y2, label='10sec LAM period', linestyle='dashed', color='green')
+    y3 = [formulas.N_max_d(T_prop(15), BW, L, d, LAM_size) for L in x]
+    plt.plot(x, y3, label='15sec LAM period', linestyle='dotted', color='blue')
+    y4 = [formulas.N_max_d(T_prop(20), BW, L, d, LAM_size) for L in x]
+    plt.plot(x, y4, label='20sec LAM period', linestyle=(0, (1, 1)), color='purple')
+    y5 = [formulas.N_max_d(T_prop(25), BW, L, d, LAM_size) for L in x]
+    plt.plot(x, y5, label='25sec LAM period', linestyle='dashdot', color='grey')
+    y6 = [formulas.N_max_d(T_prop(30), BW, L, d, LAM_size) for L in x]
     plt.plot(x, y6, label='30sec LAM period', linestyle=(0, (3, 1, 1, 1, 1, 1)), color='orange')
 
     # plt.title(f'Maximum number of nodes for time required to propagate LAMs. LAM_size = {LAM_size}')
@@ -110,12 +145,9 @@ def plot_lambda(func, saveloc, LAM_size=350):
 
 def plot_distance(saveloc, LAM_size=350):
     BW = 1024 * 1024
-    alpha = 2
-    z_0 = 10
     LAM_prd = 5
     f_prd = 0.1
     T_prop = LAM_prd * f_prd
-    lm = 0.02
 
     x = np.array(range(1000, 5001, 500))
     y1 = [formulas.N_max_d(T_prop, BW, L, 50, LAM_size) for L in x]
@@ -151,6 +183,8 @@ def plot_distance(saveloc, LAM_size=350):
 def plot_for_size(LAM_size=350):
     plot_LAM_period(formulas.N_max_assum_original, f'graphs/original/LAM_prd_{LAM_size}.pdf', LAM_size)
     plot_LAM_period(formulas.N_max_assum, f'graphs/corrected/LAM_prd_{LAM_size}.pdf', LAM_size)
+    plot_LAM_period_distance(50, f'graphs/original/LAM_prd_d50_{LAM_size}.pdf', LAM_size)
+    plot_LAM_period_distance(100, f'graphs/original/LAM_prd_d100_{LAM_size}.pdf', LAM_size)
     plot_lambda(formulas.N_max_assum_original, f'graphs/original/density_{LAM_size}.pdf', LAM_size)
     plot_lambda(formulas.N_max_assum, f'graphs/corrected/density_{LAM_size}.pdf', LAM_size)
     plot_distance(f'graphs/original/distance_{LAM_size}.pdf', LAM_size)
